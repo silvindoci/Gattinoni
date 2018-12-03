ac3e6aa6240141bacf8924cc46a8443531266ebe
silvindoci <silvesterndoci@hotmailcom>
2018-12-03 17:57:41 +0100

    l

--- js/main.js
+++ js/main.js
@@ -39,3 +39,22 @@ var dropmenu = (function() {
     return { init: init };
 
 })();
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("silverslider");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+
+
+
+function initMap() {
+    var uluru = { lat: -25.344, lng: 131.036 };
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 10, center: uluru });
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
